d1cec503882ccad3e58a9cb2c21889fc2ceb2518
neosquid <3781546+neosquid@users.noreply.github.com>
2019-07-29 22:55:05 +0800

    Add files via upload

--- Tiles/src/defines.asm
+++ Tiles/src/defines.asm
@@ -2,12 +2,15 @@
 
 IO_TileMapContr	equ $6B  		; Tile Map Control
 IO_TileMapAttr	equ $6C  		; Default Tilemap Attribute
+
 IO_TileMapBaseAdrr	equ $6E  		; Tilemap Base Address
 IO_TileMapDefBaseAddr 	equ $6F  		; Tile Definitions Base Address
 IO_TileMapPaletteContr  equ $43  		; Palette Control
+
 IO_TileMapClipWindow	equ $1B 		; Clip Window Tilemap
 IO_TileMapTransparency 	equ $4C 		; Transparency index for the tilemap
 IO_ULAControl	equ $68		; ULA Control
+
 IO_TileMapOffSetXMSB 	equ $47  		; Tilemap Offset X MSB		
 IO_TileMapOffSetXLSB 	equ $2F  		; Tilemap Offset X LSB		         		         
 IO_TileMapOffsetY	equ $31  		; Tilemap Offset Y
@@ -15,6 +18,10 @@ IO_TileMapOffsetY	equ $31  		; Tilemap Offset Y
 screen:  	   	 equ $4000 	 	
 ulaattr: 	   	 equ $5800		
 tilemap:   	   	 equ $6000 	 	
-tileMapDefAddr:	 equ tilemap+2560 	; start address of tile map definitons
+
+tileMapDefAddr:	 equ tilemap+1280 	; start address of tile map definitons
+;tileMapDefAddr:	 equ tilemap+2560 	; start address of tile map definitons
+
+
 characterData 	 equ $3D00 		; start address of characterData
 screenAddresstoPrint	 equ 20489		; address where 'Central Cavern' title will be printed
